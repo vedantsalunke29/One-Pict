05158a26abc771e3f9d42365a6166cabdec76298
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-04-23 12:35:57 +0530

    first commit One-Pict

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 app.use(cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_DEPLOY_URL],
+    origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(cookieParser())
