91ddaf687ee18ed283bb2034dd57901fb58b473d
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-04-23 12:16:44 +0530

    first commit One-Pict

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 app.use(cors({
-    origin: " https://one-pict.vercel.app"
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_DEPLOY_URL]
 }));
 app.use(cookieParser())
 app.use(fileUpload({
