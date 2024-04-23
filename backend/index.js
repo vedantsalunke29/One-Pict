90421a4050b998785408e43c8105eecb7081f9a8
Vedant <156643612+vedantsalunke29@users.noreply.github.com>
2024-04-23 18:45:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,14 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 app.use(cors());
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(cookieParser())
 app.use(fileUpload({
     useTempFiles: true,
