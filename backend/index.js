8b436203c1ff6427548dab9c23e6f27dc8436a84
Vedant <156643612+vedantsalunke29@users.noreply.github.com>
2024-04-23 11:07:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,9 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
-app.use(cors());
+app.use(cors({
+    origin : [https://one-pict.vercel.app/]
+}));
 app.use(cookieParser())
 app.use(fileUpload({
     useTempFiles: true,
