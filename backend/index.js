73a255eba5851df475173232e217ad0aee7712bc
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-04-23 11:49:12 +0530

    first commit One-Pict

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,12 @@ connectDB();
 
 const app = express();
 
-
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
-app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_DEPLOY_URL]
-}));
+app.use(cors());
 app.use(cookieParser())
 app.use(fileUpload({
     useTempFiles: true,
