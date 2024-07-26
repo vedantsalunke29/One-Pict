1b47ebc8fe5e15ea4615870569c2fac83c001156
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-07-26 23:43:20 +0530

    s

--- frontend/src/components/MovieRecommendations.jsx
+++ frontend/src/components/MovieRecommendations.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const MovieRecommendations = () => {
 	const [recommendations, setRecommendations] = useState([]);
-	const API_URL = "https://one-pict-ml.onrender.com";
+	const API_URL = "https://movie-recommendation-suz4.onrender.com";
 	const [movie, setMovie] = useState("");
 	const fetchRecommendations = async (movie_name) => {
 		try {
