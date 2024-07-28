5e51f8df7e77df4246fc21ac83ead84b2c65fadf
vedantsalunke29.Vedant <vedantsalunke29@gmail.com>
2024-07-28 14:22:03 +0530

    s

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,15 @@ import { Provider } from "react-redux";
 import store from "./store/index.jsx";
 import { inject } from "@vercel/analytics";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
-			<Analytics/>
+			<Analytics />
+			<SpeedInsights />
 		</Provider>
 	</React.StrictMode>,
 );
