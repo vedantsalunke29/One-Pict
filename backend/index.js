eb6a77d76c6f80ef68429dadd385cf26bb107a16
Vedant <156643612+vedantsalunke29@users.noreply.github.com>
2024-08-13 20:12:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 const corsConfig = {
-    origin: "*",
+    origin: "https://one-pict.vercel.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
     credential: true
 }
